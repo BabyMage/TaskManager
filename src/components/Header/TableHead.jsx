3ee65648f19db85555a209619b4ff12e6cd31f80
BabyMage <lucasfoliveira2025@gmail.com>
2026-04-09 18:32:21 -0300

    Inicio do projeto

---
+++ src/components/Header/TableHead.jsx
@@ -0,0 +1,18 @@
+// Cabeçalho da tabela
+
+function TableHead()
+{
+    return(
+        <table>
+            <thead>
+                <tr>
+                    <th>Tarefa</th>
+                    <th>Data</th>
+                    <th>Concluída</th>
+                </tr>
+            </thead>
+        </table>
+    );
+};
+
+export default TableHead;
