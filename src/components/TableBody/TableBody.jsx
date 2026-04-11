f0e8707aa15086c01020dbdec596108c94b04305
BabyMage <lucasfoliveira2025@gmail.com>
2026-04-11 16:02:52 -0300

    😏

--- src/components/TableBody/TableBody.jsx
+++ src/components/TableBody/TableBody.jsx
@@ -7,8 +7,8 @@ function TableBody({ tasks = [] })
     return(
         <table>
             <tbody>
-                {tasks.map((task, index) =>(
-                    <tr key = {index}>
+                {tasks.map((task) =>(
+                    <tr key = {task.id}>
                         <td>{task.Task}</td>
                         <td>{task.Date}</td>
                         <td>{task.Done ? "Yes" : "No"}</td>
