07d70bc4c3bd08876e3ccebe9d1801f2354bf7cd
BabyMage <lucasfoliveira2025@gmail.com>
2026-05-23 17:15:09 -0300

    <3

--- backend/Controllers/taskController.py
+++ backend/Controllers/taskController.py
@@ -1,27 +1,45 @@
-from backend.Models.taskModel import TaskModel
+from backend.Models.TaskModel import TaskModel
 
 
 class TaskController():
-    
     def __init__(self):
         self.model = TaskModel()
         
+    
+    
     def get_tasks(self):
         return self.model.get_tasks()
 
+
+
     def create_task(self, task_data):
-        
+
+        task = task_data.task.strip.title()
+
+        valid_priorities = ["Alta", "Média", "Baixa"]
+        if task_data.priority not in valid_priorities:
+            raise ValueError("Prioridade Invalida")
+
         return self.model.create_task(
-            task_data.task,
+            task,
             task_data.date,
             task_data.priority,
             task_data.category,
             task_data.done
         )
     
+
+
     def update_tasks(self, task_data, id):
+
+        task = task_data.task.strip.title()
+
+        valid_priorities = ["Alta", "Média", "Baixa"]
+        if task_data.priority not in valid_priorities:
+            raise ValueError("Prioridade Invalida")
+
         return self.model.update_task(
-            task_data.task,
+            task,
             task_data.date,
             task_data.priority,
             task_data.category,
@@ -29,5 +47,7 @@ class TaskController():
             id
         )
     
+
+
     def delete_task(self, id):
         return self.model.delete_task(id)
