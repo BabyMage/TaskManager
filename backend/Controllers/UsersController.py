4986ba6f21e6cfea76fa11ba5eac5adfcfdefb10
BabyMage <lucasfoliveira2025@gmail.com>
2026-05-29 17:05:29 -0300

    <3

--- backend/Controllers/UsersController.py
+++ backend/Controllers/UsersController.py
@@ -1,64 +1,47 @@
 from backend.Models.UsersModel import UsersModel
-from datetime import datetime, timedelta
-from dotenv import load_dotenv
-from jose import jwt
-import bcrypt
-import os
-
-
-load_dotenv()
+import backend.Security.hash as hash_handler
+import backend.Security.jwt_handler as token_handler
 
 class UserController():
     def __init__(self):
         self.model = UsersModel()
-    
-
 
     def login(self, user_data):
-
-        user = self.model.get_user_by_email(user_data.email)
+        user = self.model.get_user_by_email(
+            user_data.email
+        )
 
         if not user:
-            return {"error": "Usuario não Encontrado"}
-        
-        password_correct = bcrypt.checkpw(
-            user_data.password.encode("utf-8"),
-            user["senha"].encode("utf-8")
-        )
+            return {
+                "error": "Usuário não encontrado"
+            }
+
+        password_correct = hash_handler.verify_password(
+            user_data.password,
+            user["senha"]
+        )   
 
         if not password_correct:
-            return {"Error": "Senha Incorreta"}
-        
-        expiration = datetime.utcnow() + timedelta(hours=2)
+            return {
+                "error": "Senha incorreta"
+            }
 
-        payload = {
+        token = token_handler.create_token({
             "user_id": user["id"],
-            "email": user["email"],
-            "exp": expiration
+            "email": user["email"]
+        })
+
+        return {
+            "access_token": token
         }
-
-        token = jwt.encode(
-            payload,
-            os.getenv("SECRET_KEY"),
-            algorithm=os.getenv("ALGORITHM")
-        )
-
-        return{
-            "access_token" : token
-        }
-
-
 
     def create_user(self, user_data):
-        password_bytes = user_data.password.encode("utf-8")
-
-        salt = bcrypt.gensalt()
-        hashed_password = bcrypt.hashpw(password_bytes, salt)
-        hashed_password = hashed_password.decode("utf-8")
+        hashed_password = hash_handler.hash_password(
+            user_data.password
+        )
 
         return self.model.create_user(
             user_data.username,
             user_data.email,
             hashed_password
         )
-    
