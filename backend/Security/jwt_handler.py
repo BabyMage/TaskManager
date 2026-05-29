42cb8d908e2ce6e6589ff93c48ab7b39db1389a1
BabyMage <lucasfoliveira2025@gmail.com>
2026-05-29 17:05:29 -0300

    <3

---
+++ backend/Security/jwt_handler.py
@@ -0,0 +1,41 @@
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+from jose import jwt, JWTError
+import os
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+
+
+def create_token(data: dict) -> str:
+    payload = data.copy()
+
+    expiration = datetime.utcnow() + timedelta(hours=2)
+
+    payload.update({
+            "exp": expiration   
+        })
+    
+    token = jwt.encode(
+        payload,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
+
+    return token
+
+
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(
+            token, 
+            SECRET_KEY,
+            algorithms=[ALGORITHM]
+        )
+
+        return payload
+    
+    except JWTError:
+        return None
