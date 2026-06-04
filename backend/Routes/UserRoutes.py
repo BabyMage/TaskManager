319653f11779d435e7387eab16d562b51c7f0227
BabyMage <lucasfoliveira2025@gmail.com>
2026-06-04 15:45:27 -0300

    Proximo passo: Testes e Rotas de tarefas

---
+++ backend/Routes/UserRoutes.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, Depends
+from backend.Controllers.UsersController import UserController
+from backend.Schemas.UserSchema import CreateUserSchema, UserLoginSchema, UserUpdateSchema
+from backend.Dependencies.auth import get_current_user
+
+router = APIRouter()
+controller = UserController()
+
+
+@router.post("/register")
+def register(user_data: CreateUserSchema):
+    return controller.create_user(user_data)
+
+
+@router.post("/login")
+def login(user_data: UserLoginSchema):
+    return controller.login(user_data)
+
+
+@router.get("/me")
+def get_me(current_user = Depends(get_current_user)):
+    return current_user
+
+
+@router.put("/me")
+def update_user(
+    user_data: UserUpdateSchema,
+    current_user = Depends(get_current_user)):
+    return controller.update_user(
+        current_user["id"],
+        user_data
+    )
+
+
+@router.delete("/me")
+def delete_user(
+    current_user = Depends(get_current_user)
+):
+
+    return controller.delete_user(
+        current_user["id"]
+    )
--- backend/main.py
+++ backend/main.py
@@ -1,18 +1,6 @@
-import json
-from pathlib import Path
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-
-
-
-# 📦 Modelo da tarefa
-class Task(BaseModel):
-    Task: str
-    Date: str
-    Category: str
-    Priority: str | None = "Média"
-    Done: bool 
+from backend.Routes.UserRoutes import router as user_router
 
 
 app = FastAPI()
@@ -26,76 +14,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# 📁 Caminho do JSON
-FILE_PATH = Path(__file__).parent / "tasks.json"
 
-# -> ROTAS 👇👇
-
-
-# 🔍 GET - pegar tarefas
-@app.get("/tasks")
-def get_tasks():
-    with open(FILE_PATH, "r") as file:
-        data = json.load(file)
-    return data
-
-#################################################################
-
-# ➕ POST - adicionar tarefa
-@app.post("/add_task")
-def add_task(task: Task):
-    with open(FILE_PATH, "r") as file:
-        data = json.load(file)
-    
-    if data["list"]:
-        last_id = data["list"][-1]["id"]
-    else:
-        last_id = 0
-    
-    new_task = task.model_dump()
-    new_task["id"] = last_id + 1
-
-
-    data["list"].append(new_task)
-
-    with open(FILE_PATH, "w") as file:
-        json.dump(data, file, indent=4)
-    
-    return new_task
-
-
-
-#################################################################
-
-
-@app.put("/update_task/{task_id}")
-def update_task(task_id: int, updated_task: Task ):
-    with open(FILE_PATH, "r") as file:
-        data = json.load(file)
-
-    for i, task in enumerate(data["list"]):
-        if task["id"] == task_id:
-            updated_data = updated_task.model_dump()
-            updated_data["id"] = task_id
-            data["list"][i] = updated_data
-            break
-
-    with open(FILE_PATH, "w") as file:
-        json.dump(data, file, indent=4)
-
-
-#################################################################
-
-
-@app.delete("/delete_task/{task_id}")
-def delete_task(task_id: int):
-    with open(FILE_PATH, "r") as file:
-        data = json.load(file)
-
-    # filtra removendo a tarefa com o ID
-    data["list"] = [task for task in data["list"] if task["id"] != task_id]
-
-    with open(FILE_PATH, "w") as file:
-        json.dump(data, file, indent=4)
-
-    return {"message": "Task deletada com sucesso"}
+app.include_router(
+    user_router,
+    prefix="/users",
+    tags=["Users"]
+)
