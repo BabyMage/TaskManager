c5edbdc3c8fc6d648c99e18729fbfdd6f93e82a3
BabyMage <lucasfoliveira2025@gmail.com>
2026-08-29 16:27:45 -0300

    Proximo passo: Retrabalhar CSS

--- src/components/TableBody/TableBody.jsx
+++ src/components/TableBody/TableBody.jsx
@@ -7,45 +7,47 @@ function TableBody({ tasks, setSelectedTask })
 {  
     return(
         <tbody>
-            {tasks.map((task) =>(
-                <tr 
-                    key = {task.id}
-                    onClick={() => setSelectedTask(task)}
-                    style={{cursor: "pointer"}}
-                >
-                        
-                    <td>{task.task}</td>
-                    <td>{task.date}</td>
-                    
-                    <td>
-                        <span className={`tag ${task.category}`}>
-                            {task.category}
-                        </span>
-                    </td>
-                    
-                    <td>
-                        <span className={`priority ${task.priority}`}>
-                            {task.priority}
-                        </span>
-                    </td>
-                    
-                    <td style={{ color: task.done ? "green" : "red" }}>
-                        {task.done ? "✅Concluído" : "⏳Pendente"}
-                    </td>
+            {tasks.map((task) => {
 
-                    <td>
-                        <span
-                        style={{
-                            fontSize: "12px",
-                            background: getTaskStatus(task.date).color}}
-        
-                        className={`date-status ${status.color}`}>
-                            {getTaskStatus(task.date).label}
-                        </span>
-                    </td>
-                </tr>
-            ))}
+                const status = getTaskStatus(task.date);
+
+                return (
+                    <tr 
+                        key={task.id}
+                        onClick={() => setSelectedTask(task)}
+                    >
+                        
+                        <td>{task.task}</td>
+
+                        <td>{task.date}</td>
+                        
+                        <td>
+                            <span className={`tag ${task.category}`}>
+                                {task.category}
+                            </span>
+                        </td>
+                        
+                        <td>
+                            <span className={`priority ${task.priority}`}>
+                                {task.priority}
+                            </span>
+                        </td>
+                        
+                        <td className={task.done ? "task-done" : "task-pending"}>
+                            {task.done ? "✅ Concluído" : "⏳ Pendente"}
+                        </td>
+
+                        <td>
+                            <span className={`date-status ${status.color}`}>
+                                {status.label}
+                            </span>
+                        </td>
+
+                    </tr>
+                )
+            })}
         </tbody>      
-)}
+    )
+}
 
 export default TableBody;
